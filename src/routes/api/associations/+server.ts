8f6433783481db30569c27379af5e9a65083b008
Léon Muselli <leon.muselli@gmail.com>
2025-10-17 14:06:31 +0200

    Feat: Fetch associations from database and return as JSON

--- src/routes/api/associations/+server.ts
+++ src/routes/api/associations/+server.ts
@@ -1,7 +1,19 @@
 import type { RequestEvent } from "@sveltejs/kit";
+import db from "$lib/server/database";
+import type { AssociationDB } from "$lib/databasetypes";
 
 export const GET = async (event: RequestEvent) => {
 
+    const associations: AssociationDB[] = await db`
+        SELECT
+            *
+        FROM
+            associations
+    `;
+
+    return new Response(JSON.stringify(associations), {
+        headers: { "Content-Type": "application/json" }
+    });
     
     
 };
