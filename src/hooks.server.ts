d12e688c5b3eb2218756f506e8802fc74182e40d
Léon Muselli <leon.muselli@gmail.com>
2025-10-17 14:06:31 +0200

    Refactor: Move database and auth logic to server-side modules

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,12 @@
-import { handle as authHandle } from '$lib/auth';
-import db from "$lib/database";
+import { handle as authHandle } from "$lib/server/auth";
+import db from "$lib/server/database";
 
 export const handle = async ({ event, resolve }) => {
 	// Tracking page view
 	try {
 		const path = event.url.pathname;
-		const session = event.locals?.auth && await event.locals.auth();
-		const userId = session?.user?.id || null;
+		const session = event.locals?.auth && (await event.locals.auth());
+		const userId = session?.user?.id || "anonymous";
 		db`
 			INSERT INTO page_views (path, user_id)
 			VALUES (${path}, ${userId})
