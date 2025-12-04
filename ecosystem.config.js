159b063b3c84ba1c92941cd3d0f80ab3307ac210
Léon Muselli <leon.muselli@etu.emse.fr>
2025-12-04 10:46:59 +0100

    pm2 env loading

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	apps: [
+		{
+			name: "portail-etu",
+			script: "bun build/index.js",
+			env: {
+				PORT: 3000,
+				ORIGIN: "https://portail-etu.emse.fr",
+				BODY_SIZE_LIMIT: "10485760", // 10MB
+			},
+		},
+	],
+};
